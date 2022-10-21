afd89506c31399cbc2ae9bb77c6636f83399bec7
redmont <deepanshusingh@Deepanshus-MacBook-Pro.local>
2022-10-22 04:23:08 +0530

    simple event management contract

--- migrations/02-lottery-deployment.js
+++ migrations/02-lottery-deployment.js
@@ -1,4 +1,6 @@
 const Lottery = artifacts.require("lottery");
+const EventManagement = artifacts.require("event_management");
 module.exports = function (deployer) {
     deployer.deploy(Lottery);
+    deployer.deploy(EventManagement);
 };
